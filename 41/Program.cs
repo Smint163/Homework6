5e6c64dff5ecc40f2a2953031ae21440b1e4708f
Smint <smint-russia@mail.ru>
2023-01-03 23:15:53 +0300

    for checking

---
+++ 41/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223 -> 3
+*/
+
+Console.WriteLine("Введите целые числа через ПРОБЕЛ");
+
+int[] number = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int summ = 0;
+
+for (int i = 0; i < number.Length; i++)
+        {
+            if (number[i] > 0)
+                {
+                summ++;
+                }
+
+        Console.Write($"{number[i]}, ");
+        }
+
+Console.Write($"-> {summ}");
